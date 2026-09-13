cfddbd353468e4ff7aceef327ec0700c2e89802b
ub42t1za0d <marmot-franker8v@icloud.com>
2026-09-13 13:06:15 +0000

    Fix b4fc233538a0

--- internal/api/apitask/apitask.go
+++ internal/api/apitask/apitask.go
@@ -69,7 +69,7 @@ func Run(ctx context.Context, task Task) error {
 // Wait will wait for all currently running request-scoped background tasks to
 // complete before returning.
 func Wait(ctx context.Context) {
-	wrk, ok := from(context.Background())
+	wrk, ok := from(ctx)
 	if !ok {
 		return
 	}
